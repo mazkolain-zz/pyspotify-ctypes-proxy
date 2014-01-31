ecad1c943d4343248021f1d182c3afac973542b9
Mikel Azkolain <azkotoki@gmail.com>
2014-01-31 16:07:18 +0100

    Updated package version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyspotifyCtypesProxy',
-    version='0.4',
+    version='0.5',
     description='An embedded proxy server intended for players that cannot integrate libspotify easily.',
     author='Mikel Azkolain',
     author_email='azkotoki@gmail.com',
